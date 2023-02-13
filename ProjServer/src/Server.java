ca149f794ccbbad1b6a2798ae265ff5d6e54d4e7
Ashumaa <122601185+Ashumaa@users.noreply.github.com>
2023-02-13 08:46:10 +0100

    Update Server.java

--- ProjServer/src/Server.java
+++ ProjServer/src/Server.java
@@ -15,70 +15,53 @@ public class Server {
     public void startServer(int port) throws IOException {
     	
         serverSocket = new ServerSocket(port);
-
-        System.out.println("Il server e' partito!");
+        System.out.println("Server: ON");
         System.out.println("IP: 10.0.1.1");
-        System.out.println("Porta: 9876");
-        
+        System.out.println("Porta: 8888");
         clientSocket = serverSocket.accept();
-
         System.out.println("Client connesso correttamente!");
     }
 
     public void riceviComandi() throws IOException {
 
     	Movement = new Comandi();
-    	
         output = new PrintWriter(clientSocket.getOutputStream(), true);
         input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
         while (true) {
             char c = (char)input.read();
             System.out.println("Client: " + c);
-             
            switch (c)
            {
-            
-            case '0':
-            	
-            	System.out.println("rallenta");	
+            case '0':	
+            	System.out.println("slow");	
             	Movement.slow();;
-            	break;
-            	
-               
-            	
+            	break;   	
             case 'w':
-            	System.out.println("avanti");
+            	System.out.println("forward");
             	Movement.goForward();
             	break;
-            	
             case 's':
-            	System.out.println("indietro");
+            	System.out.println("backward");
             	Movement.goBackward();
             	break;
-            	
             case 'a':
-            	System.out.println("sinistra");
+            	System.out.println("left");
             	Movement.rotateLeft();
             	break;
-            	
             case 'd':
-            	System.out.println("destra");
+            	System.out.println("right");
             	Movement.rotateRight();
             	break;
-            	
             case 'q':
-            	System.out.println("accelera");
+            	System.out.println("speed up");
             	Movement.setMaxSpeed();
             	break;
-            	
             case 27:
             	System.out.println("Spegnimento server!");
             	Movement.shutDown();
             	stopServer();
             	break;
            }
-            
             getMidVel();
         }
     }
@@ -86,12 +69,9 @@ public class Server {
     public void getMidVel() {
     	
         output.println(Movement.getMidSpeed());
-  
-        
     }
     
     public void stopServer() throws IOException {
-    	
     	clientSocket.close();
     	serverSocket.close();
     	output.close();
@@ -100,13 +80,10 @@ public class Server {
 
 
         public static void main(String[] args) throws IOException {
-
             Server server = new Server();
-
-            server.startServer(9876);
+            server.startServer(8888);
             server.riceviComandi();
-            
             server.getMidVel();
             
         }
-}
+    }
